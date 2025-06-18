07089cdf250f468e0384eaa4d7b49031437178b9
Lana-vanuza <lanavanuzadasilva@gmail.com>
2025-06-18 14:11:08 -0300

    Create carregar-bloco.js

---
+++ carregar-bloco.js
@@ -0,0 +1,11 @@
+// carregar-bloco.js
+fetch('bloco-produto-novo.html')
+  .then(response => response.text())
+  .then(html => {
+    const container = document.createElement('div');
+    container.innerHTML = html;
+    document.querySelector('main').appendChild(container);
+  })
+  .catch(error => {
+    console.error('Erro ao carregar bloco novo:', error);
+  });
