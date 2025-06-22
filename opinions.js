daba6813732de31d4214f98196a19f9a4970a50c
Lana-vanuza <lanavanuzadasilva@gmail.com>
2025-06-22 16:26:32 -0300

    Update opinions.js

--- opinions.js
+++ opinions.js
@@ -8,18 +8,18 @@ window.addEventListener('DOMContentLoaded', () => {
   imagens.forEach(img => {
     img.addEventListener("click", () => {
       modalImg.src = img.src;
-      modal.style.display = "flex";
+      modal.classList.add("active");
     });
   });
 
   fechar.addEventListener("click", () => {
-    modal.style.display = "none";
+    modal.classList.remove("active");
   });
 
-  // Fecha ao clicar fora da imagem
+  // Também fecha ao clicar fora da imagem
   modal.addEventListener("click", (e) => {
     if (e.target === modal) {
-      modal.style.display = "none";
+      modal.classList.remove("active");
     }
   });
 });
