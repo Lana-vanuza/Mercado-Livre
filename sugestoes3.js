d7243573e958e2bf50158addd00385e93f060599
Lana-vanuza <lanavanuzadasilva@gmail.com>
2025-06-24 08:09:03 -0300

    Create sugestoes3.js

---
+++ sugestoes3.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementById('sugestoes-container'); // usa o que já existe no HTML
+
+  fetch('sugestoes1.html')
+    .then(response => response.text())
+    .then(html => {
+      container.innerHTML = html;
+    })
+    .catch(error => {
+      console.error('Erro ao carregar sugestões:', error);
+    });
+});
